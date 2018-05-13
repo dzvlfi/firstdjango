cd7255bfdcda8877b1d41da18f8e826f88889cc1
Dzulfqar Ridha <dzulfi.ridha@gmail.com>
2018-05-13 13:14:28 +0700

    class-based view, using template view

--- delivery/views.py
+++ delivery/views.py
@@ -1,45 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 import random
 
 # Create your views here.
-def home_old(request):
-	html_var = 'aku' #f_strings
-	html_ = f"""<!doctype html>
-	<html lang="en">
-	<head>
-	</head>
-	<body>
-	<h1>Hello World!</h1>
-	<p>{html_var} di bandung, dingin euy hehe</p>
-	</body>
-	</html>
-	"""
-	return HttpResponse(html_) 
+class HomeView(TemplateView):
+	template_name = 'home.html'
 
-def home(request):
-	num = None
-	some_list = [
-		random.randint(0, 1000),
-		random.randint(0, 1000),
-		random.randint(0, 1000)
-	]
-	condition_bool_item = True
-	if condition_bool_item:
-		num = random.randint(0, 1000)
-	context = {
-		"num": num,
-		"some_list": some_list,
-		"html_var": "hai,"
-	}
-	return render(request, 'home.html', context) #response, with context
+	def get_context_data(self, *args, **kwargs):
+		context = super(HomeView, self).get_context_data(*args,**kwargs)
+		num = None
+		some_list = [
+			random.randint(0, 1000),
+			random.randint(0, 1000),
+			random.randint(0, 1000)
+		]
+		condition_bool_item = True
+		if condition_bool_item:
+			num = random.randint(0, 1000)
+		context = {
+			"num": num,
+			"some_list": some_list,
+			"html_var": "hai,"
+		}
+		return context
 
-def about(request):
-	context = {
-	}
-	return render(request, 'about.html', context) #response, with context
-
-def contact(request):
-	context = {
-	}
-	return render(request, 'contact.html', context) #response, with context
+class ContactView(TemplateView):
+	template_name = 'contact.html'
+ 
--- diliva/urls.py
+++ diliva/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from delivery.views import home, about, contact
+from delivery.views import HomeView, ContactView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
-    url(r'^about/$', about),
-    url(r'^contact/$', contact),
+    url(r'^$', HomeView.as_view()),
+    url(r'^about/$', TemplateView.as_view(template_name = 'about.html')),
+    url(r'^contact/$', ContactView.as_view()),
 ]
